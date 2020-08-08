98244381895833e81b602d4a51a3a0dcb2840e5d
61423 <chinguun9903@gmail.com>
2020-08-09 02:44:51 +1000

    Backend Set up

---
+++ BackEnd/src/main/java/com/majorproject2/majorproject2thurs18302/Majorproject2Thurs18302Application.java
@@ -0,0 +1,13 @@
+package com.majorproject2.majorproject2thurs18302;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Majorproject2Thurs18302Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Majorproject2Thurs18302Application.class, args);
+    }
+
+}
---
+++ BackEnd/src/test/java/com/majorproject2/majorproject2thurs18302/Majorproject2Thurs18302ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.majorproject2.majorproject2thurs18302;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Majorproject2Thurs18302ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
