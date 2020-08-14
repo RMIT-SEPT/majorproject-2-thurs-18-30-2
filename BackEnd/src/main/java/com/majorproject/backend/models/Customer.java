1990f89dcdc89c42c7acc96e3ab563b0619d3e4f
61423 <chinguun9903@gmail.com>
2020-08-14 22:05:27 +1000

    S0-Email Query Function For Customer

--- BackEnd/src/main/java/com/majorproject/backend/models/Customer.java
+++ BackEnd/src/main/java/com/majorproject/backend/models/Customer.java
@@ -11,7 +11,7 @@ public class Customer extends User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    
     public Customer() {
     }
 
--- BackEnd/src/main/java/com/majorproject/backend/services/CustomerService.java
+++ BackEnd/src/main/java/com/majorproject/backend/services/CustomerService.java
@@ -12,9 +12,13 @@ public class CustomerService {
     private CustomerRepository customerRepository;
 
 
-    public Customer saveOrUpdatePerson(Customer person) {
+    public Customer saveOrUpdateCustomer(Customer person) {
 
         //logic
         return customerRepository.save(person);
+    }
+
+    public Customer getCustomerByEmail(String email) {
+        return customerRepository.findByEmail(email);
     }
 }
