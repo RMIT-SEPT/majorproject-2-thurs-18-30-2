e852801525d6eab121dddbf2d1f0c92622abd285
Danial_Isman_s3786400 <s3786400@student.rmit.edu.au>
2020-09-03 14:14:44 +1000

    S1-Input Checking: username input checking

--- FrontEnd/frontend/src/components/inputs/UsernameInput.js
+++ FrontEnd/frontend/src/components/inputs/UsernameInput.js
@@ -1,20 +1,48 @@
 import React from 'react';
 import { Form, Row, Col } from 'react-bootstrap';
+import _ from 'lodash';
 
 class UsernameInput extends React.Component {
     constructor (props) {
         super(props);
         this.state = {
-            value : ''
+            value : '',
+            valid : false,
+            border : "1px solid black",
+            errMsg : ""
         };
 
         this.handleChange = this.handleChange.bind(this);
     }
 
+    
+
     handleChange (event) {
         this.setState({
             value : event.target.value
         });
+
+        
+
+        event.persist();
+
+        if (this.debounceFn) {
+            this.debounceFn.cancel()
+            this.debounceFn = false
+        }
+
+        this.debounceFn = _.debounce(() => {
+            //insert api call
+
+            if(!this.valid) {
+                this.setState({
+                    border : '1px solid red',
+                    errMsg : "Username already exists, please try a different username"
+                });
+            }
+        }, 700, { leading : false, trailing: true });
+
+        this.debounceFn();
     }
 
     render () {
@@ -26,7 +54,10 @@ class UsernameInput extends React.Component {
                     {this.props.naming}
                 </Form.Label>
                 <Col sm={this.props.pos[1]}>
-                    <Form.Control placeholder="Username" type="Username" value={this.state.value} onChange={this.handleChange} />
+                    <Form.Control style={{border: this.state.border}} placeholder="Username" type="Username" value={this.state.value} onChange={this.handleChange} />
+                    <p3 style={{color: 'red', fontSize: '12px'}}>
+                        {this.state.errMsg}
+                    </p3>
                 </Col>
             </Form.Group>
         );
