5bc0af03c63a6b937316d6c959be7ca6216f09b1
Danial_Isman_s3786400 <s3786400@student.rmit.edu.au>
2020-09-27 19:56:38 +1000

    S2-Make booking template

--- FrontEnd/frontend/src/components/WorkingTimes.js
+++ FrontEnd/frontend/src/components/WorkingTimes.js
@@ -12,23 +12,102 @@ import {
   ConfirmationDialog,
 } from '@devexpress/dx-react-scheduler-material-ui';
 import { appointments } from './appointments';
+import { Form } from 'react-bootstrap';
 
-export default class WorkingTimes extends React.PureComponent {
+
+
+
+const messages = {
+    moreInformationLabel: '',
+  };
+
+
+const TextEditor = (props) => {
+// eslint-disable-next-line react/destructuring-assignment
+if (props.type === 'multilineTextEditor') {
+    return null;
+} return <AppointmentForm.TextEditor {...props} />;
+};
+
+const ong = (props) => {
+    if (props.type === 'multilineTextEditor') {
+      return null;
+    } return <AppointmentForm.Select {...props} />;
+  };
+  
+  const BasicLayout = ({ onFieldChange, appointmentData, ...restProps }) => {
+    const onCustomFieldChange = (nextValue) => {
+      onFieldChange({ customField: nextValue });
+    };
+  
+    return (
+      <AppointmentForm.BasicLayout
+        appointmentData={appointmentData}
+        onFieldChange={onFieldChange}
+        {...restProps}
+      >
+        <AppointmentForm.Label
+          text="Service"
+          type="title"
+        />
+        <AppointmentForm.Select
+          value={appointmentData.customField}
+          availableOptions={[
+            {
+                id : 0,
+                text : "Full Body Massage"
+            },
+            {
+                id : 1,
+                text : "Super Happy Fun Time"
+            },
+            {
+                id : 2,
+                text : "Designer Designing Designs"
+            }
+        ]}
+          onValueChange={onCustomFieldChange}
+          placeholder="Custom field"
+        />
+      </AppointmentForm.BasicLayout>
+    );
+  };
+
+
+class WorkingTimes extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: appointments,
-      currentDate: '2020-09-27',
-
-      addedAppointment: {},
-      appointmentChanges: {},
-      editingAppointment: undefined,
+      data : appointments,
+      currentDate : '2020-09-27',
+      addedAppointment : {},
+      appointmentChanges : {},
+      editingAppointment : undefined,
+      service : '0',
+      employee : '0',
+      startTime : 8,
+      endTime : 18
     };
 
     this.commitChanges = this.commitChanges.bind(this);
     this.changeAddedAppointment = this.changeAddedAppointment.bind(this);
     this.changeAppointmentChanges = this.changeAppointmentChanges.bind(this);
     this.changeEditingAppointment = this.changeEditingAppointment.bind(this);
+    this.handleChangeService = this.handleChangeService.bind(this);
+    this.handleChangeEmployee = this.handleChangeEmployee.bind(this);
+  }
+
+  handleChangeService(event) {
+    this.setState({
+        service : event.target.value
+    });
+  }
+
+  handleChangeEmployee(event) {
+    this.setState({
+        employee : event.target.value
+    });
+    console.log("test");
   }
 
   changeAddedAppointment(addedAppointment) {
@@ -65,43 +144,88 @@ export default class WorkingTimes extends React.PureComponent {
     const {
       currentDate, data, addedAppointment, appointmentChanges, editingAppointment,
     } = this.state;
-
     return (
-      <Paper>
-        <Scheduler
-          data={data}
-          height={660}
-        >
-          <ViewState
-            currentDate={currentDate}
-          />
-          <EditingState
-            onCommitChanges={this.commitChanges}
+        <React.Fragment>
+            <br></br>
+            <br></br>
+            <Form.Group controlId="ServiceSelection">
+                <Form.Label>Select the service you would like to make a booking for</Form.Label>
+                {/* Insert api call here and map to see all services that are available (maybe hardcode) | <br> are for testing purposes only*/}
+                
+                <Form.Control as="select" onChange={this.handleChangeService}>
+                    <option value='0'>-services-</option>
+                    <option value='1'>1</option>
+                    <option value='2'>2</option>
+                    <option value='3'>3</option>
+                    <option value='4'>4</option>
+                    <option value='5'>5</option>
+                </Form.Control>
+            </Form.Group>
+            <br></br>
+            <br></br>
+            <br></br>
 
-            addedAppointment={addedAppointment}
-            onAddedAppointmentChange={this.changeAddedAppointment}
+            {this.state.service !== '0' &&
+                <Form.Group controlId="EmployeeSelection">
+                    <Form.Label>Select the employee you would like to make a booking for</Form.Label>
+                    {/* Insert api call here and map to see all employees that are available */}
+                    <Form.Control as="select" onChange={this.handleChangeEmployee}>
+                        <option value='0'>-employees-</option>
+                        <option value='1'>1</option>
+                        <option value='2'>2</option>
+                        <option value='3'>3</option>
+                        <option value='4'>4</option>
+                        <option value='5'>5</option>
+                    </Form.Control>
+                </Form.Group>   
+            }
+            <br></br>
+            <br></br>
+            <br></br>
+            {this.state.employee !== '0' &&
+                <Paper>
+                    <Scheduler
+                    data={data}
+                    height={500}
+                    >
+                    <ViewState
+                        currentDate={currentDate}
+                    />
+                    <EditingState
+                        onCommitChanges={this.commitChanges}
 
-            appointmentChanges={appointmentChanges}
-            onAppointmentChangesChange={this.changeAppointmentChanges}
+                        addedAppointment={addedAppointment}
+                        onAddedAppointmentChange={this.changeAddedAppointment}
 
-            editingAppointment={editingAppointment}
-            onEditingAppointmentChange={this.changeEditingAppointment}
-          />
-          <WeekView
-            startDayHour={9}
-            endDayHour={17}
-          />
-          <AllDayPanel />
-          <EditRecurrenceMenu />
-          <ConfirmationDialog />
-          <Appointments />
-          <AppointmentTooltip
-            showOpenButton
-            showDeleteButton
-          />
-          <AppointmentForm />
-        </Scheduler>
-      </Paper>
+                        appointmentChanges={appointmentChanges}
+                        onAppointmentChangesChange={this.changeAppointmentChanges}
+
+                        editingAppointment={editingAppointment}
+                        onEditingAppointmentChange={this.changeEditingAppointment}
+                    />
+                    <WeekView
+                        startDayHour={this.state.startTime}
+                        endDayHour={this.state.endTime}
+                    />
+                    <AllDayPanel />
+                    <EditRecurrenceMenu />
+                    <ConfirmationDialog />
+                    <Appointments />
+                    <AppointmentTooltip
+                        showOpenButton
+                        showDeleteButton
+                    />
+                    <AppointmentForm 
+                        basicLayoutComponent={BasicLayout}
+                        textEditorComponent={TextEditor}
+                        selectComponent={ong}
+                        messages={messages}
+                    />
+                    </Scheduler>
+                </Paper>
+            }
+        </React.Fragment>
     );
   }
 }
+export default WorkingTimes;
