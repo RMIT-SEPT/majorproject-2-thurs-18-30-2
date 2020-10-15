62f8984d37afa511ccc956437ef7221e4cff2352
Danial_Isman_s3786400 <s3786400@student.rmit.edu.au>
2020-10-15 15:17:44 +1100

    S4-Startup service error fixed

--- FrontEnd/frontend/src/components/BookingForm.js
+++ FrontEnd/frontend/src/components/BookingForm.js
@@ -7,8 +7,6 @@ import {
   AppointmentForm,
   AppointmentTooltip,
   WeekView,
-  ViewSwitcher,
-  Toolbar,
   MonthView,
   EditRecurrenceMenu,
   AllDayPanel,
@@ -20,6 +18,68 @@ import { openModal } from '../app/reducers/modalSlice';
 import { connect } from 'react-redux';
 import api from '../app/api';
 import '../css/BookingForm.css';
+
+
+
+
+const messages = {
+    moreInformationLabel: '',
+  };
+
+
+const TextEditor = (props) => {
+// eslint-disable-next-line react/destructuring-assignment
+if (props.type === 'multilineTextEditor') {
+    return null;
+} return <AppointmentForm.TextEditor {...props} />;
+};
+
+const ong = (props) => {
+    if (props.type === 'multilineTextEditor') {
+      return null;
+    } return <AppointmentForm.Select {...props} />;
+  };
+  
+  const BasicLayout = ({ onFieldChange, appointmentData, ...restProps }) => {
+    const onCustomFieldChange = (nextValue) => {
+      onFieldChange({ customField: nextValue });
+    };
+  
+    return (
+      <AppointmentForm.BasicLayout
+        appointmentData={appointmentData}
+        onFieldChange={onFieldChange}
+        {...restProps}
+      >
+        <AppointmentForm.Label
+          text="Service"
+          type="title"
+        />
+        <AppointmentForm.Select
+          value={appointmentData.customField}
+          availableOptions={[
+            {
+                id : 0,
+                text : "Full Body Massage"
+            },
+            {
+                id : 1,
+                text : "Super Happy Fun Time"
+            },
+            {
+                id : 2,
+                text : "Designer Designing Designs"
+            }
+        ]}
+          onValueChange={onCustomFieldChange}
+          placeholder="Custom field"
+        />
+      </AppointmentForm.BasicLayout>
+    );
+  };
+
+
+
 
 class BookingForm extends React.Component {
   constructor(props) {
@@ -38,9 +98,12 @@ class BookingForm extends React.Component {
       style : {backgroundColor: '#FFC107', borderRadius: '8px'},
       services : [],
       employees : [],
-      modal : { title : "Booking Successfull", body : "Thanks for choosing us, you have successfully made a booking" }
+      modal : { title : "Booking Successful", body : "Thanks for choosing us, you have successfully made a booking" }
     };
 
+    this.commitChanges = this.commitChanges.bind(this);
+    this.changeAddedAppointment = this.changeAddedAppointment.bind(this);
+    this.changeAppointmentChanges = this.changeAppointmentChanges.bind(this);
     this.handleChangeService = this.handleChangeService.bind(this);
     this.handleChangeEmployee = this.handleChangeEmployee.bind(this);
     this.appointmentClickHandler = this.appointmentClickHandler.bind(this);
@@ -49,6 +112,7 @@ class BookingForm extends React.Component {
     this.makeBookings = this.makeBookings.bind(this);
 
     this.state.currentDate = new Date();
+    console.log("FWEIOUFBUAEIWOFBER");
   }
 
   async componentDidMount() {
@@ -67,26 +131,21 @@ class BookingForm extends React.Component {
 
   makeBookings() {
     var tempBuffer = this.state.bookingBuffer;
-    if(tempBuffer.length > 0) {
-      tempBuffer.forEach(element => 
-        api.post('booking/create', element)
-              .then((response) => {
-                  console.log(response.data);
-              }).catch((error) => {
-                  this.setState({
-                      valid : false,
-                      errorMsg : error.response.data.message
-                  });
-              })
+    tempBuffer.forEach(element => 
+      api.post('booking/create', element)
+            .then((response) => {
+                this.setState({
+                  bookingBuffer : []
+                })
+            }).catch((error) => {
+                this.setState({
+                    valid : false,
+                    errorMsg : error.response.data.message
+                });
+            })
             );
-    }
-    else {
-        this.setState({
-          modal : { title : "No Bookings Selected", body : "Please select at least 1 booking and try again" }
-        });
     this.props.openModal(this.state.modal);
   }
-}
 
   appointmentComponent(props) {
     var [style, setStyle] = useState(this.state.style)
@@ -127,18 +186,16 @@ class BookingForm extends React.Component {
     this.setState({
         serviceID : event.target.value
     });
-    
     event.persist();
     if(event.target.value !== -1) {
-      api.get('employeeSchedule/getEmployees/bService/' + event.target.value)
+      api.get('employeeSchedule/getBServices/employee/bService/' + event.target.value)
               .then((response) => {
-                console.log(response.data)
-              }).catch((error) => {
-                  this.setState({ 
-                      errorMsg : error.response.data.message
+                  this.setState({
+                      employees : response.data
                   });
+              }).catch((error) => {
+                  alert("No Employees Available For This Service");
               });
-              
     }
     else {
       this.setState({
@@ -151,7 +208,7 @@ class BookingForm extends React.Component {
   handleChangeEmployee(event) {
     event.persist();
     if(event.target.value !== -1) {
-      api.get('employeeSchedule/getSchedules/employee/bService/' + event.target.value + '/' + this.state.serviceID)
+      api.get('employeeSchedule/getSchedules/employee/available/' + event.target.value)
           .then((response) => {
               var tempData = [];
               response.data.forEach((element, index) =>
@@ -180,6 +237,33 @@ class BookingForm extends React.Component {
     }
   }
 
+  changeAddedAppointment(addedAppointment) {
+    this.setState({ addedAppointment });
+  }
+
+  changeAppointmentChanges(appointmentChanges) {
+    this.setState({ appointmentChanges });
+  }
+
+
+  commitChanges({ added, changed, deleted }) {
+    this.setState((state) => {
+      let { data } = state;
+      if (added) {
+        const startingAddedId = data.length > 0 ? data[data.length - 1].id + 1 : 0;
+        data = [...data, { id: startingAddedId, ...added }];
+      }
+      if (changed) {
+        data = data.map(appointment => (
+          changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment));
+      }
+      if (deleted !== undefined) {
+        data = data.filter(appointment => appointment.id !== deleted);
+      }
+      return { data };
+    });
+  }
+
   render() {
     
     if(this.props.user.userDetails) {
@@ -194,7 +278,6 @@ class BookingForm extends React.Component {
                         this.state.services.map(
                         function(data) {
                           if(data) {
-                            console.log(data);
                             return(<option key={data.id} value={data.id}>{data.name}</option>);
                           }
                         }
@@ -214,7 +297,6 @@ class BookingForm extends React.Component {
                             this.state.employees.map(
                               function(data) {
                                 if(data) {
-                                  console.log(data);
                                   return(<option key={data.employeeId} value={data.employeeId}>{data.employeeFName}</option>);
                                 }
                               }
@@ -234,16 +316,32 @@ class BookingForm extends React.Component {
                         <ViewState
                             currentDate={this.state.currentDate}
                         />
-                        <EditingState />
+                        <EditingState
+                            onCommitChanges={this.commitChanges}
+
+                            addedAppointment={this.state.addedAppointment}
+                            onAddedAppointmentChange={this.changeAddedAppointment}
+
+                            appointmentChanges={this.state.appointmentChanges}
+                            onAppointmentChangesChange={this.changeAppointmentChanges}
+
+                        />
                         <MonthView />
-                        <WeekView />
                         <AllDayPanel />
                         <EditRecurrenceMenu />
                         <ConfirmationDialog />
-                        <Toolbar />
-                        <ViewSwitcher />
                         <Appointments 
                           appointmentComponent={this.appointmentComponent}
+                        />
+                        <AppointmentTooltip
+                            showOpenButton
+                            showDeleteButton
+                        />
+                        <AppointmentForm 
+                            basicLayoutComponent={BasicLayout}
+                            textEditorComponent={TextEditor}
+                            selectComponent={ong}
+                            messages={messages}
                         />
                         </Scheduler>
                     </Paper>
